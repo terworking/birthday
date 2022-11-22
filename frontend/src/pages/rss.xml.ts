2e58554eff83eaa74891d052c917ae4e135dcc26
Aynh <99479536+Aynh@users.noreply.github.com>
2022-11-22 15:58:54 +0800

    feat: implement RSS API route

---
+++ frontend/src/pages/rss.xml.ts
@@ -0,0 +1,37 @@
+import rss from '@astrojs/rss'
+import type { APIRoute } from 'astro'
+import { subYears } from 'date-fns'
+
+import { fetchBirthdayNotificationServer } from '~utils/backend'
+import { calculateNextBirthdayDate } from '~utils/birthday'
+
+export const get: APIRoute = async (context) => {
+  const items = await fetchBirthdayNotificationServer('list')
+    .then((response) => response.json())
+    .then((values: BirthdayData[]) =>
+      values
+        .map((value) => {
+          const pastBirthday = subYears(calculateNextBirthdayDate(value), 1)
+          const age = pastBirthday.getFullYear() - value.year
+
+          return { age, name: value.name, pastBirthday }
+        })
+        .sort(
+          ({ pastBirthday: a }, { pastBirthday: b }) =>
+            b.valueOf() - a.valueOf()
+        )
+    )
+
+  const site = context.url.origin
+  return rss({
+    description: 'Past birthday history, sorted by recent',
+    title: 'Birthday History',
+    site,
+    items: items.map(({ age, name, pastBirthday }) => ({
+      link: site,
+      pubDate: pastBirthday,
+      title: `${name} birthday`,
+      description: `${name} turns ${age} today`,
+    })),
+  })
+}
