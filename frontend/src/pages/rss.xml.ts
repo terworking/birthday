9e87541e39bcaa2c69c83848123e2bace94282ea
Aynh <99479536+Aynh@users.noreply.github.com>
2022-11-22 16:55:22 +0800

    chore: debug error

--- frontend/src/pages/rss.xml.ts
+++ frontend/src/pages/rss.xml.ts
@@ -6,33 +6,37 @@ import { fetchBirthdayNotificationServer } from '~utils/backend'
 import { calculateNextBirthdayDate } from '~utils/birthday'
 
 export const get: APIRoute = async (context) => {
-  const items = await fetchBirthdayNotificationServer('list')
-    .then((response) => response.json())
-    .then((values: BirthdayData[]) =>
-      values
-        .map((value) => {
-          const pastBirthday = subYears(calculateNextBirthdayDate(value), 1)
-          const age = pastBirthday.getFullYear() - value.year
+  try {
+    const items = await fetchBirthdayNotificationServer('list')
+      .then((response) => response.json())
+      .then((values: BirthdayData[]) =>
+        values
+          .map((value) => {
+            const pastBirthday = subYears(calculateNextBirthdayDate(value), 1)
+            const age = pastBirthday.getFullYear() - value.year
 
-          return { age, name: value.name, pastBirthday }
-        })
-        .sort(
-          ({ pastBirthday: a }, { pastBirthday: b }) =>
-            b.valueOf() - a.valueOf()
-        )
-    )
+            return { age, name: value.name, pastBirthday }
+          })
+          .sort(
+            ({ pastBirthday: a }, { pastBirthday: b }) =>
+              b.valueOf() - a.valueOf()
+          )
+      )
 
-  const site = context.url.origin
-  return rss({
-    description: 'Past birthday history, sorted by recent',
-    title: 'Birthday History',
-    site,
-    items: items.map(({ age, name, pastBirthday }) => ({
-      link: site,
-      pubDate: pastBirthday,
-      title: `${name} birthday`,
-      description: `${name} turns ${age} today`,
-    })),
-    stylesheet: '/rss/pretty-feed-v3.xsl',
-  })
+    const site = context.url.origin
+    return rss({
+      description: 'Past birthday history, sorted by recent',
+      title: 'Birthday History',
+      site,
+      items: items.map(({ age, name, pastBirthday }) => ({
+        link: site,
+        pubDate: pastBirthday,
+        title: `${name} birthday`,
+        description: `${name} turns ${age} today`,
+      })),
+      stylesheet: '/rss/pretty-feed-v3.xsl',
+    })
+  } catch (e) {
+    return new Response(JSON.stringify(e), { status: 500 })
+  }
 }
