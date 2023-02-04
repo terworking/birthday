5a12e9a6d13cf4738ed5c996d40297afd4b222f8
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-02-04 20:14:53 +0800

    feat: trigger cron using cloudflare worker

---
+++ worker-cron/src/index.js
@@ -0,0 +1,19 @@
+export default {
+	async scheduled(_, env, ctx) {
+		ctx.waitUntil(
+			fetch(
+				`https://api.github.com/repos/${env.OWNER}/${env.REPO}/actions/workflows/trigger.yaml/dispatches`,
+				{
+					body: JSON.stringify({ ref: 'main' }),
+					method: 'POST',
+					headers: {
+						Accept: 'application/vnd.github+json; charset=utf-8',
+						Authorization: `Bearer ${env.TOKEN}`,
+						'X-GitHub-Api-Version': '2022-11-28',
+						'User-Agent': 'worker-cron'
+					}
+				}
+			)
+		);
+	}
+};
