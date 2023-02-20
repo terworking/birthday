045c99b5ed41e9ba390100d2c383a82c7d78b589
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-02-21 01:08:22 +0800

    install unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import Icons from 'unplugin-icons/vite';
 import type { UserConfig } from 'vite';
+import Icons from 'unplugin-icons/vite';
+import { presetUno } from 'unocss';
+import UnoCSS from 'unocss/vite';
 
 const config: UserConfig = {
 	define: { 'import.meta.vitest': 'undefined' },
-	plugins: [sveltekit(), Icons({ compiler: 'svelte', defaultClass: 't-icon', scale: 1 })]
+	plugins: [
+		UnoCSS({ mode: 'svelte-scoped', presets: [presetUno()] }),
+		sveltekit(),
+		Icons({ compiler: 'svelte', defaultClass: 't-icon', scale: 1 })
+	]
 };
 
 export default config;
