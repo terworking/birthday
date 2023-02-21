3fcd3cd6b78bb53e8f7feea35ca37ec8d6023707
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-02-21 16:33:46 +0800

    feat: use svelte's goto in footer navigation

--- src/lib/AppFooter.svelte
+++ src/lib/AppFooter.svelte
@@ -1,30 +1,17 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { getContext } from 'svelte';
 	import type { Writable } from 'svelte/store';
 	import type { State } from './types';
 
-	interface FooterIcon {
+	interface FooterLink {
 		icon: string;
 		title: string;
 		href: string;
 	}
 
-	const state = getContext('state') as Writable<State>;
-
-	$: ({ selectedKey } = $state);
-	$: icons = <FooterIcon[]>[
-		$page.route.id === '/'
-			? {
-					icon: 'counter',
-					title: 'Counter page',
-					href: `/countdown/${selectedKey}`
-			  }
-			: {
-					icon: 'home',
-					title: 'Index page',
-					href: `/?select=${selectedKey}`
-			  },
+	$: links = <FooterLink[]>[
 		{
 			icon: 'discord',
 			title: 'Discord server invite',
@@ -46,26 +33,41 @@
 			href: 'https://www.youtube.com/@terworking'
 		}
 	];
+
+	const state = getContext('state') as Writable<State>;
+
+	$: isIndex = $page.route.id === '/';
+	const navigate = async () => {
+		const url = new URL($page.url);
+		const key = $state.selectedKey;
+
+		if (isIndex) {
+			url.pathname = `/countdown/${key}`;
+		} else {
+			url.pathname = '/';
+			url.searchParams.set('select', key);
+		}
+
+		await goto(url);
+	};
 </script>
 
 <footer class="flex flex-col items-center">
 	<div class="w-full flex justify-around">
-		{#each icons as { icon, href, title }}
-			{@const [rel, target] = href.startsWith('https') ? ['noreferrer', '_blank'] : []}
-			<a {rel} {target} {title} {href}> <div class={`t-icon ${icon}`} /> </a>
+		<button
+			on:click={() => navigate()}
+			class:i-simple-icons-counterstrike={isIndex}
+			class:i-mdi-home={!isIndex}
+			class="t-icon cursor-pointer"
+			title={`${isIndex ? 'Counter' : 'Index'} page`}
+		/>
+		{#each links as { icon, href, title }}
+			<a rel="noreferrer" target="_blank" {title} {href}> <div class={`t-icon ${icon}`} /> </a>
 		{/each}
 	</div>
 </footer>
 
 <style>
-	.home {
-		--uno: 'i-mdi-home';
-	}
-
-	.counter {
-		--uno: 'i-simple-icons-counterstrike';
-	}
-
 	.discord {
 		--uno: 'i-simple-icons-discord';
 	}
