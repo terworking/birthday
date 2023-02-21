250de34eaa168d42b3e82076f5b9f84018966537
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-02-21 15:28:49 +0800

    feat: accept target key as countdown parameter

--- src/routes/countdown/[name]/+page.server.ts
+++ src/routes/countdown/[name]/+page.server.ts
@@ -5,8 +5,11 @@ import type { PageServerLoad } from './$types';
 
 const normalizeName = (name: string) => name.toLowerCase().replaceAll(' ', '-');
 
-export const load = (({ params: { name }, url }) => {
-	let matchIndex = targets.findIndex((value) => normalizeName(value.name) === name);
+export const load = (({ params, url }) => {
+	let matchIndex = targets.findIndex(
+		(value) =>
+			birthdayTargetAsKey(value) === params.name || normalizeName(value.name) === params.name
+	);
 
 	if (matchIndex === -1) {
 		throw error(404);
@@ -22,10 +25,16 @@ export const load = (({ params: { name }, url }) => {
 	}
 
 	const match = targets.at(matchIndex % targets.length)!;
+
+	const key = birthdayTargetAsKey(match);
+	if (key === params.name) {
+		shouldRedirect = true;
+	}
+
 	if (shouldRedirect) {
 		const name = normalizeName(match.name);
 		throw redirect(301, `/countdown/${name}`);
 	}
 
-	return { target: { key: birthdayTargetAsKey(match), ...match } };
+	return { target: { key, ...match } };
 }) satisfies PageServerLoad;
