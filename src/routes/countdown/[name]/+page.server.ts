9bc19a117116fbfc4878a0f89fd38f389c3e5d70
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-02-21 15:12:53 +0800

    feat: add previous and next button to countdown page

--- src/routes/countdown/[name]/+page.server.ts
+++ src/routes/countdown/[name]/+page.server.ts
@@ -1,13 +1,30 @@
 import { targets } from '$lib/server/target';
 import { birthdayTargetAsKey } from '$lib/util';
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load = (({ params: { name } }) => {
-	const match = targets.find((value) => value.name.toLowerCase().replaceAll(' ', '-') === name);
+const normalizeName = (name: string) => name.toLowerCase().replaceAll(' ', '-');
 
-	if (match === undefined) {
+export const load = (({ params: { name }, url }) => {
+	let matchIndex = targets.findIndex((value) => normalizeName(value.name) === name);
+
+	if (matchIndex === -1) {
 		throw error(404);
+	}
+
+	let shouldRedirect = false;
+	if (url.searchParams.get('previous') === '1') {
+		matchIndex -= 1;
+		shouldRedirect = true;
+	} else if (url.searchParams.get('next') === '1') {
+		matchIndex += 1;
+		shouldRedirect = true;
+	}
+
+	const match = targets.at(matchIndex % targets.length)!;
+	if (shouldRedirect) {
+		const name = normalizeName(match.name);
+		throw redirect(301, `/countdown/${name}`);
 	}
 
 	return { target: { key: birthdayTargetAsKey(match), ...match } };
--- src/routes/countdown/[name]/+page.svelte
+++ src/routes/countdown/[name]/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import BirthdayCountdown from '$lib/components/BirthdayCountdown.svelte';
 	import BirthdaySubscribe from '$lib/components/BirthdaySubscribe.svelte';
 	import type { State } from '$lib/types';
@@ -10,9 +12,25 @@
 
 	const state = getContext('state') as Writable<State>;
 	$state.selectedKey = data.target.key;
+
+	const navigate = async (to: 'next' | 'previous') => {
+		const url = new URL($page.url);
+		url.searchParams.set(to, '1');
+		await goto(url);
+	};
 </script>
 
-<div>
-	<BirthdayCountdown {data} />
-	<BirthdaySubscribe {data} />
+<div class="flex items-center justify-between">
+	<button on:click={() => navigate('previous')} class="t-icon i-mdi-pan-left" />
+	<div class="flex-1">
+		<BirthdayCountdown {data} />
+		<BirthdaySubscribe {data} />
+	</div>
+	<button on:click={() => navigate('next')} class="t-icon i-mdi-pan-right" />
 </div>
+
+<style>
+	button {
+		--uno: 'cursor-pointer';
+	}
+</style>
