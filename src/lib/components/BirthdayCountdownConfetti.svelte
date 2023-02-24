0705d57a1fe417718c506772e7630e4b4b60e019
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-02-25 01:58:37 +0800

    feat: better confetti placements

--- src/lib/components/BirthdayCountdownConfetti.svelte
+++ src/lib/components/BirthdayCountdownConfetti.svelte
@@ -3,26 +3,36 @@
 	import { loadConfettiPreset } from 'tsparticles-preset-confetti';
 
 	let screenWidth: number;
+	$: desktop = screenWidth >= 1024;
+
+	type Option = Record<'x' | 'y', number> & { direction: string };
+	$: options = (
+		desktop
+			? [
+					// show particles from above the body
+					{ direction: 'top', x: 20, y: -5 },
+					{ direction: 'top', x: 80, y: -5 },
+			  ]
+			: [
+					// show particles from left and right side of the body
+					{ direction: 'top-right', x: -5, y: 20 },
+					{ direction: 'top-left', x: 105, y: 20 },
+			  ]
+	) satisfies [Option, Option];
+
 	$: rate = (
-		screenWidth > 1024
+		desktop
 			? // show more particles on desktop
 			  { delay: 0.1, quantity: 10 }
 			: { delay: 0.2, quantity: 5 }
 	) satisfies Record<'delay' | 'quantity', number>;
 
-	$: emitters = [
-		{ positionX: 5, moveDirection: 'top-right' },
-		{ positionX: 95, moveDirection: 'top-left' },
-	].map(({ positionX, moveDirection }) => {
-		return {
-			rate,
-			life: { count: 0 },
-			position: { x: positionX, y: 20 },
-			particles: {
-				move: { direction: moveDirection },
-			},
-		};
-	});
+	$: emitters = options.map(({ direction, ...position }) => ({
+		particles: { move: { direction } },
+		position,
+		rate,
+		life: { count: 0 }, // show indefinitely
+	}));
 </script>
 
 <svelte:window bind:innerWidth={screenWidth} />
