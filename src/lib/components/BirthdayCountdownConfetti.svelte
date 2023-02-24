88e85c4df711d59f1b7ab4de7e4bfd535098fd3e
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-02-25 01:32:01 +0800

    feat: more particles on desktop

--- src/lib/components/BirthdayCountdownConfetti.svelte
+++ src/lib/components/BirthdayCountdownConfetti.svelte
@@ -1,31 +1,35 @@
 <script lang="ts">
 	import Particles from 'svelte-particles';
 	import { loadConfettiPreset } from 'tsparticles-preset-confetti';
+
+	let screenWidth: number;
+	$: rate = (
+		screenWidth > 1024
+			? // show more particles on desktop
+			  { delay: 0.1, quantity: 10 }
+			: { delay: 0.2, quantity: 5 }
+	) satisfies Record<'delay' | 'quantity', number>;
+
+	$: emitters = [
+		{ positionX: 5, moveDirection: 'top-right' },
+		{ positionX: 95, moveDirection: 'top-left' },
+	].map(({ positionX, moveDirection }) => {
+		return {
+			rate,
+			life: { count: 0 },
+			position: { x: positionX, y: 20 },
+			particles: {
+				move: { direction: moveDirection },
+			},
+		};
+	});
 </script>
+
+<svelte:window bind:innerWidth={screenWidth} />
 
 <Particles
 	id="tsparticles"
-	options={{
-		preset: 'confetti',
-		emitters: [
-			{
-				life: { count: 0 },
-				position: { x: 5, y: 20 },
-				particles: {
-					move: { direction: 'top-right' },
-				},
-				rate: { delay: 0.2, quantity: 5 },
-			},
-			{
-				life: { count: 0 },
-				position: { x: 95, y: 20 },
-				particles: {
-					move: { direction: 'top-left' },
-				},
-				rate: { delay: 0.2, quantity: 5 },
-			},
-		],
-	}}
+	options={{ preset: 'confetti', emitters }}
 	particlesInit={loadConfettiPreset}
 />
 
